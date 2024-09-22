6be35af34bc22a1359f6f42c4b3719385aaea533
Humayra-Amin <humaema4576@gmail.com>
2024-09-22 14:35:01 +0600

    Discover added with responsive

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,10 +1,8 @@
 import Discover from "../Discover/Discover";
-import Overview from "../Overview/Overview";
 
 const HomePage = () => {
     return (
         <div>
-            {/* <Overview></Overview> */}
             <Discover></Discover>
         </div>
     );
