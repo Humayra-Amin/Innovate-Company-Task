2fe837c57163f02bcd3b6bb84a39ecf88eb78ac7
Humayra-Amin <humaema4576@gmail.com>
2024-09-22 11:37:59 +0600

    Navbar added with responsive

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,80 +1,55 @@
-import Link from 'next/link';
+"use client"
+import { useState } from 'react';
+import { FaSearch, FaChevronDown, FaBars } from 'react-icons/fa';
 
 const Navbar = () => {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setIsOpen(!isOpen);
+    };
 
     return (
-        <div className='fixed top-0 left-0 right-0 z-50 shadow-md'>
-            <div className="navbar bg-gray-500 border-2 border-gray-500 shadow-lg">
-                <div className="navbar-start px-8">
-                    <div className="dropdown">
-                        <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-                            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" />
-                            </svg>
-                        </div>
-                        <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow rounded-box w-52 text-white bg-gray-500 playfair gap-3">
-                        {navItems.map((item) => (
-                                <Link className="font-semibold hover:text-primary hover:border-gray-200 hover:p-1 hover:rounded-xl hover:bg-gray-200 duration-300"
-                                    href={item.path}
-                                    key={item.path}
-                                >
-                                    {item.title}
-                                </Link>
-                            ))}
-                        </ul>
-                    </div>
-                    <Link href={'/'} className="btn btn-ghost text-2xl text-white playwrite">
-                        Humayra<span className='text-red-600 text-4xl mb-2'>.</span>
-                    </Link>
-                </div>
-                <div className="navbar-center hidden lg:flex">
-                    <ul className="menu menu-horizontal px-1 text-white lg:text-[18px]">
-                        <div className='flex items-center space-x-6 playfair'>
-                            {navItems.map((item) => (
-                                <Link className="font-semibold hover:text-primary hover:border-gray-200 hover:p-1 hover:rounded-xl hover:bg-gray-200 duration-300"
-                                    href={item.path}
-                                    key={item.path}
-                                >
-                                    {item.title}
-                                </Link>
-                            ))}
-                        </div>
-                    </ul>
-                </div>
+        <div className="bg-[#363636] text-white py-4 px-6 md:px-12 lg:px-24 flex justify-between items-center">
+
+            {/* Hamburger */}
+            <div className="md:hidden">
+                <button onClick={toggleMenu}>
+                    <FaBars className="text-xl" />
+                </button>
+            </div>
+
+            {/* Navigation */}
+            <div className={`space-x-0 md:space-x-10 lg:space-x-28 text-lg flex flex-col md:flex-row items-center justify-center absolute md:static w-[50%] md:w-auto bg-[#363636] md:bg-transparent transition-all duration-300 ease-in-out ${isOpen ? 'top-[85px]' : 'top-[-300px]'}`}>
+                <a href="#home" className="flex items-center space-x-3 py-2 md:py-0">
+                    <span>Home</span>
+                    <FaChevronDown className="text-xs" />
+                </a>
+                <a href="#event" className="flex items-center space-x-2 py-2 md:py-0">
+                    <span>Event</span>
+                    <FaChevronDown className="text-xs" />
+                </a>
+                <a href="#contact" className="flex items-center space-x-2 py-2 md:py-0">
+                    <span>Contact</span>
+                    <FaChevronDown className="text-xs" />
+                </a>
+                <a href="#blog" className="flex items-center space-x-2 py-2 md:py-0">
+                    <span>Blog</span>
+                    <FaChevronDown className="text-xs" />
+                </a>
+            </div>
+
+            {/* Search */}
+            <div className="flex items-center bg-white rounded-full w-[200px] md:w-[200px] mt-4 md:mt-0">
+                <input
+                    type="text"
+                    placeholder="Search"
+                    className="bg-transparent outline-none text-[#000000] placeholder-gray-300 p-2 text-sm md:text-xl w-full"
+                />
+                <FaSearch className="text-black w-8 h-8 md:w-10 md:h-10 lg:w-10 lg:h-10 p-2"/>
             </div>
         </div>
     );
 };
-
-const navItems = [
-    {
-        title: "Home",
-        path: "/"
-    },
-    {
-        title: "Skills",
-        path: "/skills"
-    },
-    {
-        title: "Services",
-        path: "/service"
-    },
-    {
-        title: "Projects",
-        path: "/project"
-    },
-    {
-        title: "Experiences",
-        path: "/experience"
-    },
-    {
-        title: "Blog",
-        path: "/blog"
-    },
-    {
-        title: "Contact",
-        path: "/contact"
-    },
-];
 
 export default Navbar;
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,11 +1,7 @@
-import Navbar from "@/components/Navbar/Navbar";
-
-
 
 const HomePage = () => {
     return (
         <div className="bg-gradient-to-b from-gray-200 to-gray-50">
-            <Navbar></Navbar>
         </div>
     );
 };
