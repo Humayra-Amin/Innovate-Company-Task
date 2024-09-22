f4c454830402f2a5e84e358f72800bd194ba0c6f
Humayra-Amin <humaema4576@gmail.com>
2024-09-22 15:06:47 +0600

    overview added with responsive

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,8 +1,10 @@
 import Discover from "../Discover/Discover";
+import Overview from "../Overview/Overview";
 
 const HomePage = () => {
     return (
         <div>
+            <Overview></Overview>
             <Discover></Discover>
         </div>
     );
